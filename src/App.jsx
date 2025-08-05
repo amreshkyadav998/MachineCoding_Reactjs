3f70dc1d9aa1317b799fa8198b6fa38103603a4c
Amresh Yadav <amreshky998@gmail.com>
2025-08-05 14:37:37 +0530

    star rating

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,19 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import Weather from './components/Weather'
 import Todo from './components/Todo'
+import StarRating from './components/StarRating'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-     <div className='p-6 border-2 text-3xl border-red-400 flex items-center justify-center bg-amber-50'>Hello Everyone this is the Todo application in reactjs</div>
-     <Weather />
-     <Todo/>
+     <div className='p-6 border-2 text-3xl border-red-400 flex items-center justify-center bg-amber-50'>Hello Everyone this is the Star Rating application in reactjs</div>
+     {/* <Weather />
+     <Todo/> */}
+
+      <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h2>⭐ Rate this product</h2>
+      <StarRating totalStars={5} />
+    </div>
     </>
   )
 }
